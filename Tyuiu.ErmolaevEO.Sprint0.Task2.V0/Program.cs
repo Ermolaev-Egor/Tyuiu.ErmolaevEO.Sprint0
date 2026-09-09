747368dfe2890b53c8b1d6a3cf13993d2902795d
lolkaya86 <jkfjermolaev@yandex.ru>
2026-09-09 20:15:53 +0500

    Task2

---
+++ Tyuiu.ErmolaevEO.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ErmolaevEO.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ErmolaevEO.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Егор"));
+            Console.ReadKey();
+        }
+    }
+}   
